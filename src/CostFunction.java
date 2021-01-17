ad05010dfd9f916cde3f21b281d1edd4330d748b
aldo-manco-jr <aldo.manco@protonmail.ch>
2021-01-17 11:06:18 +0100

    library_problem_fixed_script_engine_added

--- src/CostFunction.java
+++ src/CostFunction.java
@@ -1,9 +1,22 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 /**
  * Created by Aldo Manco on 1/16/2021.
  */
 public class CostFunction {
 
     public static double f(double x, double y) {
+
+        /*ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        String foo = "Math.exp(-" + x + " * " + x + " - " + y + " * " + y + ")";
+
+        try {
+            System.out.println(engine.eval(foo));
+        } catch (Exception ignored) {
+        }*/
+
         return Math.exp(-x * x - y * y) * Math.sin(x);
     }
 }
